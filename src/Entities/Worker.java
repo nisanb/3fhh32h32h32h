d274140d51736464323aacaa39e1e960294b1515
Shai Gutman <Shai Gutman@ShaiGutman-PC>
2017-02-11 00:36:37 +0200

    Worker Class was created

--- src/Entities/Worker.java
+++ src/Entities/Worker.java
@@ -5,10 +5,45 @@
  */
 package Entities;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author nisan
  */
 public class Worker {
+    private int id;
+    private String name;
+    private HashMap<Shift, Double> favoriteShifts;
+
+    public Worker(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.favoriteShifts = new HashMap<Shift, Double>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getShiftRate(Shift shiftNum) {
+        return favoriteShifts.get(shiftNum);
+    }
     
+    public void addShiftRate(Shift shiftNum, Double shiftRate) {
+        favoriteShifts.put(shiftNum, shiftRate);
+    }
 }
